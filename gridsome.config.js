d9bc653e9bd60980dcd1487becaf82c49d74b32d
Nick Lewis <nickeblewis@gmail.com>
2019-08-08 06:57:59 +0100

    routed solutions

--- gridsome.config.js
+++ gridsome.config.js
@@ -114,6 +114,25 @@ module.exports = {
       }
     },
     {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'content/solutions/**/*.md',
+        typeName: 'Post',
+        refs: {
+          tags: {
+            typeName: 'Tag',
+            route: 'tag/:id',
+            create: true
+          }
+        },
+        remark: {
+          plugins: [
+            [ 'gridsome-plugin-remark-shiki', { theme: 'Material-Theme-Palenight', skipInline: true } ]
+          ]
+        }
+      }
+    },
+    {
       use: 'gridsome-plugin-rss',
       options: {
         contentTypeName: 'Post',
