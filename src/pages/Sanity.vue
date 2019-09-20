966c8badc3764f5355650d9aa6dc044f7596fd31
nicklewiskier <nick.lewis2@kier.co.uk>
2019-09-20 14:10:57 +0100

    images now in index

--- src/pages/Sanity.vue
+++ src/pages/Sanity.vue
@@ -1,8 +1,16 @@
 <template>
   <Layout>
+        <div class="flex flex-wrap justify-center sm:w-full md:w-full lg:w-full xl:w-full">
+
     <div class="container-inner mx-auto py-16">
-      <div v-for="post in $page.posts.edges" :key="post.id" class="post border-gray-400 border-b mb-12">
+      <div v-for="post in $page.posts.edges" :key="post.id" class="bg-background-tertiary sm:w-full md:w-full lg:w-2/5 xl:w-2/5 m-3 rounded-lg shadow-lg overflow-hidden">
         <h2 class="text-3xl font-bold"><g-link :to="post.node.slug.current" class="text-copy-primary">{{ post.node.title }}</g-link></h2>
+        <g-image
+        alt="Cover image"
+        v-if="post.node.mainImage"
+        class="post-card__image"
+        :src="$urlForImage(post.node.mainImage, $page.metadata.sanityOptions).height(440).width(800).auto('format').url()"
+      />
         <div class="text-copy-secondary mb-4">
           <span>{{ post.node.publishedAt }}</span>
           <span> &middot; </span>
@@ -25,12 +33,19 @@
         :currentPage="$page.posts.pageInfo.currentPage"
       />
     </div>
+        </div>
   </Layout>
 </template>
 
 <page-query>
 query SanityPosts {
-  posts: allSanityPost {
+      metadata {
+    sanityOptions {
+      projectId
+      dataset
+    }
+  }
+  posts: allSanityPost (sortBy: "publishedAt") {
     totalCount
     pageInfo {
       totalPages
@@ -40,7 +55,28 @@ query SanityPosts {
       node {
         id
         title
-        publishedAt (format: "MMMM D, Y")        
+        publishedAt (format: "MMMM D, Y")      
+        _rawExcerpt
+        mainImage {
+          asset {
+            _id
+            url
+          }
+          caption
+          alt
+          hotspot {
+            x
+            y
+            height
+            width
+          }
+          crop {
+            top
+            bottom
+            left
+            right
+          }
+        }  
         slug {
           current
         }
