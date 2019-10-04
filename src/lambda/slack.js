e249c5d0b7f33013cf0c81095fe3039202ec7d94
Nick Lewis <nickeblewis@gmail.com>
2019-10-04 17:54:38 +0100

    new post

--- src/lambda/slack.js
+++ src/lambda/slack.js
@@ -7,7 +7,7 @@ exports.handler = async (event, context) => {
   // When the method is POST, the name will no longer be in the event’s
   // queryStringParameters – it’ll be in the event body encoded as a queryString
   const params = querystring.parse(event.body);
-  const name = params.name || "World";
+  const name = params.name || "Sanity.io";
 
   // Send greeting to Slack
   return fetch(process.env.SLACK_WEBHOOK_URL, {
@@ -15,7 +15,7 @@ exports.handler = async (event, context) => {
       "content-type": "application/json"
     },
     method: "POST",
-    body: JSON.stringify({ text: `${name} says hello!` })
+    body: JSON.stringify({ text: `${name} has published something new!` })
   })
     .then(() => ({
       statusCode: 200,
