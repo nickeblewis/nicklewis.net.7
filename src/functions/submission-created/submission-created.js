2b0bee96e781d5500d08487f612bb6bd478f9be8
nicklewiskier <nick.lewis2@kier.co.uk>
2019-11-15 15:10:12 +0000

    ewrwe

--- src/functions/submission-created/submission-created.js
+++ src/functions/submission-created/submission-created.js
@@ -1,16 +1,16 @@
-const dotenv = require('dotenv')
+require('dotenv').config()
 const fetch = require('node-fetch')
 const { EMAIL_TOKEN } = process.env
-exports.handler = async function(event, context) { 
-  const email = JSON.parse(event.body).payload.data.email
-  
+exports.handler = async event => {
+  const email = JSON.parse(event.body).payload.email
+  console.log(`Recieved a submission: ${email}`)
   return fetch('https://api.buttondown.email/v1/subscribers', {
     method: 'POST',
     headers: {
       Authorization: `Token ${EMAIL_TOKEN}`,
       'Content-Type': 'application/json',
     },
-    body: email,
+    body: JSON.stringify({ email }),
   })
     .then(response => response.json())
     .then(data => {
