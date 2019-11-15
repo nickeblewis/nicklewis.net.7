bdc17739af885bf733146a3b351e653dd0902a33
nicklewiskier <nick.lewis2@kier.co.uk>
2019-11-15 12:38:24 +0000

    pateience

--- src/functions/submission/submission.js
+++ src/functions/submission/submission.js
@@ -3,8 +3,6 @@ const fetch = require('node-fetch')
 const { EMAIL_TOKEN } = process.env
 exports.handler = async function(event, context) {
   dotenv.config()
-  console.log(JSON.parse(event.body))
-  //const email = JSON.parse(event.body).payload.email
   const email = "nick.nick@gmail.com"
   console.log(`Recieved a submission: ${email}`)
   return fetch('https://api.buttondown.email/v1/subscribers', {
