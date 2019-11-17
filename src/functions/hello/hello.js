8cc1c130cd346a8c0e4dd91fa0f4ec03db658a80
Nick Lewis <nickeblewis@gmail.com>
2019-11-17 01:00:15 +0000

    god knows

--- src/functions/hello/hello.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event, context) => {
-  try {
-    const { email } = JSON.parse(event.body);
-    console.log("test", JSON.stringify(event))
-    return { statusCode: 200, body: `Hello from a serverless function!` };
-  } catch (err) {
-    return { statusCode: 500, body: err.toString() };
-  }
-};
